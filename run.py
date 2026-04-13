921b3be445a584643538083fbaf975e1e5d08173
EZavarina <166813211+KatuhaZ@users.noreply.github.com>
2026-04-13 13:18:32 +0300

    Updated run.py

--- run.py
+++ run.py
@@ -2,12 +2,21 @@ import sys
 from datetime import datetime, timedelta
 from src.pipeline import ETLPipeline
 
+from src.utils.date import validate_date
+
 if __name__ == '__main__':
 
     # Берём даты из командной строки или используем по умолчанию
-    yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')
-    dt1 = sys.argv[1] if len(sys.argv) > 1 else yesterday
-    dt2 = sys.argv[2] if len(sys.argv) > 2 else dt1
+    if len(sys.argv) == 1:
+        yesterday = datetime.now() - timedelta(days=1)
+        dt1 = yesterday
+    else:
+        dt1 = datetime.strptime(validate_date(sys.argv[1]), '%Y-%m-%d %H:%M:%S.%f')
+
+    dt2 = sys.argv[2] if len(sys.argv) > 2 else (dt1 + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')
+    dt1 = dt1.strftime('%Y-%m-%d %H:%M:%S')
+
+    print(dt1, dt2)
 
     # Запускаем пайплайн
     pipeline = ETLPipeline()
